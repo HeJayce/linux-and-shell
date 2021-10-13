99d6faef4fc4576bfb8dc7687cce69debd3123e5
HeJayce <hejayce@gmail.com>
2021-10-13 11:30:02 +0800

    新增数组和传参

---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#定义数组
+array_a=(1 2 3 4 5)
+echo "${array_a[@]}"
+echo "${array_a[3]}"
+
+length=${#array_a[*]}
+echo ${length}
--- str.sh
+++ str.sh
@@ -9,3 +9,6 @@ greeting_2='hello, '$your_name' !'
 greeting_3='hello, ${your_name} !'
 echo ${your_name:1:4}
 echo ${#greeting_2}  $greeting_3
+echo ${your_name:0-2:1}
+read -r "asd" num 
+echo ${num}
