17144f9a2ca7f0e650dd692383cd70c0b8bbb5f4
Jayce he <hejayce@gmail.com>
2021-11-09 11:06:35 +0800

    ecs_update

--- oss_auto_upload_log/oss_auto_up.sh
+++ oss_auto_upload_log/oss_auto_up.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-date=$(date -d "yesterday" +%Y-%m-%d)
+date=$(date +%Y-%m-%d)
 # filename="/egova/apache-tomcat-7.0.105/logs/catalina.${date}.out"
 filename="catalina.${date}.out"
-echo ${filename}
-# bash oss.sh put ${filename}
+cd /egova/apache-tomcat-7.0.105/logs
+#cat ${filename}
+pwd
+bash /root/shell/oss.sh put ${filename}
