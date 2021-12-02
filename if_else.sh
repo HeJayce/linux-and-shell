23f2a5e969b16a9d728bbcc120dd773f4dc10415
Jayce he <hejayce@gmail.com>
2021-12-02 08:53:52 +0800

    完善
    完善

--- if_else.sh
+++ if_else.sh
@@ -1,12 +1,29 @@
 #!/bin/bash 
-printf "Input a number: "
-read a
-printf "Input b number: "
-read b
+# printf "Input a number: "
+# read a
+# printf "Input b number: "
+# read b
 
-if ((${a}==${b}));then 
-    echo "a=b"
-else 
-    echo "a!=b"
+# if ((${a}==${b}));then 
+#     echo "a=b"
+# else 
+#     echo "a!=b"
+# fi
+
+read age
+if test $age <= 2; then
+    echo "婴儿"
+elif test $age -ge 3 && test $age -le 8; then
+    echo "幼儿"
+elif [ $age -ge 9 ] && [ $age -le 17 ]; then
+    echo "少年"
+elif [ $age -ge 18 ] && [ $age -le 25 ]; then
+    echo "成年"
+elif test $age -ge 26 && test $age -le 40; then
+    echo "青年"
+elif test $age -ge 41 && [ $age -le 60 ]; then
+    echo "中年"
+else
+    echo "老年"
 fi
     
--- str.sh
+++ str.sh
@@ -14,3 +14,20 @@ echo ${your_name:0-2:1}
 
 variable=$(ifconfig)
 echo $variable
+
+
+read str1
+read str2
+#检测字符串是否为空
+if [ -z $str1 ] || [ -z $str2 ]
+then
+    echo "字符串不能为空"
+    exit 0
+fi
+#比较字符串
+if [ $str1 = $str2 ]
+then
+    echo "两个字符串相等"
+else
+    echo "两个字符串不相等"
+fi
