b52b5e510459f39d6ed8b856108a6a7455a1f4c0
Jayce he <hejayce@gmail.com>
2022-03-14 14:54:31 +0800

    Create mysql_install2.sh

---
+++ mysql_install2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#清理旧版mysql，或者rhel\centos自带的mariadb
+# mysqlstat=$(rpm -qa | grep mysql)
+# mariadbstat=$(rpm -qa | grep mariadb)
+# if [ ${mysqlstat} ]; then
+#     rpm -e ${mysqlstat} --nodeps
+# fi
+# if [ ${mariadbstat} ]; then
+#     rpm -e ${mariadbstat} --nodeps
+# fi
+# rm -rf /etc/my.cnf
+# rm -rf /etc/mysql
+libnumastat=$(rpm -qa | grep libnuma)
+libaiostat=$(rpm -qa *numactl*)
+if [ ! ${libnumastat} ]; then
+    yum -y install libnuma  
+fi
+if [ ! ${libaiostat} ]; then
+    yum -y install numactl
+fi
+
+
