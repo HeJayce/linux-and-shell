e5b2ad772657e9a66fab816cba259befcf5710b2
Jayce he <hejayce@gmail.com>
2022-03-29 16:43:31 +0800

    fix bug

--- jenkins_watchdog.sh
+++ jenkins_watchdog.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 while true; do
     jenkins_status=$(ps -ef | grep jenkins | grep -v grep)
-    [ ! "$jenkins_status" ] && echo $(date +%Y%m%d%H%M%S)  >> /root/log/jenkins.log
+    if [ ! "$jenkins_status" ]; then
+    echo $(date +%Y%m%d%H%M%S)  >> /root/log/jenkins.log
     systemctl restart jenkins
     echo  $(date +%Y-%m-%-d-%H:%M:%S)  | mailx -s "jenkins 异常" he@jayce.icu 2>> /dev/null
     sleep 10s
+  fi
 done
