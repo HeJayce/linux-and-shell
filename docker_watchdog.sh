714df5b56cbd480ec72ea1e39f92faaa874a56d5
Jayce he <hejayce@gmail.com>
2022-03-29 14:16:51 +0800

    docker和jenkins守护

--- docker_watchdog.sh
+++ docker_watchdog.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-while true 
-do {
-    
-    docker_images_status_exited=$(docker ps -a|grep Exited|grep -v mysql)
-    #docker_images_status_exited=$(docker ps -a|grep Exited)
+while true; do
+   {
+    status=docker ps -a | grep Exited | grep -v mysql | awk '{print $7}'
+    docker_images_status_exited=$(docker ps -a | grep Exited | grep -v mysql | awk '{print $NF}')
+    #docker_images_status_exited=$(docker ps -a|grep Exited|awk '{print $NF}')
     # [ "$docker_images_status_exited" ] && echo $(date +%Y%m%d%H%M%S) $docker_images_status_exited >> docker.txt
     if [ "$docker_images_status_exited" ]; then
-         echo  $(date +%Y%m%d%H%M%S) $docker_images_status_exited
+         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status >> docker.log
+         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status | mailx -s "docker 异常" he@jayce.icu 2>> /dev/null
     fi
-    
     sleep 300s
-}
+  }
 done
-
