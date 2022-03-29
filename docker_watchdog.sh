dd6244458c89f1cd6a5a78021b13b9eaaf3c8ee4
Jayce he <hejayce@gmail.com>
2022-03-29 10:30:56 +0800

    docker监控
    docker监控

--- docker_watchdog.sh
+++ docker_watchdog.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 while true 
 do {
+    
     docker_images_status_exited=$(docker ps -a|grep Exited|grep -v mysql)
-    [ "$docker_images_status_exited" ] && echo $docker_images_status_exited >> docker.txt
-    sleep 3600s
+    #docker_images_status_exited=$(docker ps -a|grep Exited)
+    # [ "$docker_images_status_exited" ] && echo $(date +%Y%m%d%H%M%S) $docker_images_status_exited >> docker.txt
+    if [ "$docker_images_status_exited" ]; then
+         echo  $(date +%Y%m%d%H%M%S) $docker_images_status_exited
+    fi
+    
+    sleep 300s
 }
 done
 
