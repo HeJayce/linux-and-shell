da813eb7976b297d1702bc350c49019637e5411c
Jayce he <hejayce@gmail.com>
2022-03-29 16:36:35 +0800

    watchdog

--- docker_watchdog.sh
+++ docker_watchdog.sh
@@ -6,7 +6,7 @@ while true; do
     #docker_images_status_exited=$(docker ps -a|grep Exited|awk '{print $NF}')
     # [ "$docker_images_status_exited" ] && echo $(date +%Y%m%d%H%M%S) $docker_images_status_exited >> docker.txt
     if [ "$docker_images_status_exited" ]; then
-         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status >> docker.log
+         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status >> /root/log/docker.log
          echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status | mailx -s "docker 异常" he@jayce.icu 2>> /dev/null
     fi
     sleep 300s
--- jenkins_watchdog.sh
+++ jenkins_watchdog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
     jenkins_status=$(ps -ef | grep jenkins | grep -v grep)
-    [ ! "$jenkins_status" ] && echo $(date +%Y%m%d%H%M%S)  >> jenkins.log
+    [ ! "$jenkins_status" ] && echo $(date +%Y%m%d%H%M%S)  >> /root/log/jenkins.log
     systemctl restart jenkins
     echo  $(date +%Y-%m-%-d-%H:%M:%S)  | mailx -s "jenkins 异常" he@jayce.icu 2>> /dev/null
     sleep 10s
