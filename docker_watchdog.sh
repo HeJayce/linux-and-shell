1e2ed336761492ab437733c9266b7c224fe2561d
Jayce he <hejayce@gmail.com>
2022-03-30 10:29:47 +0800

    cron

--- docker_watchdog.sh
+++ docker_watchdog.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-while true; do
-   {
+# while true; do
+#    {
     status=docker ps -a | grep Exited | grep -v mysql | awk '{print $7}'
     docker_images_status_exited=$(docker ps -a | grep Exited | grep -v mysql| grep -v docker_watchdog | awk '{print $NF}')
     #docker_images_status_exited=$(docker ps -a|grep Exited|awk '{print $NF}')
     # [ "$docker_images_status_exited" ] && echo $(date +%Y%m%d%H%M%S) $docker_images_status_exited >> docker.txt
     if [ "$docker_images_status_exited" ]; then
-         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status >> /root/log/docker.log
+         echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status >> /root/logs/docker.log
          echo  $(date +%Y-%m-%-d-%H:%M:%S) $docker_images_status_exited $status | mailx -s "docker 异常" he@jayce.icu 2>> /dev/null
     fi
-    sleep 300s
-  }
-done
+  # }
+# done
--- jenkins_watchdog.sh
+++ jenkins_watchdog.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-while true; do
-    jenkins_status=$(ps -ef | grep jenkins | grep -v grep|grep -v jenkins_watchdog)
+# while true; do
+    jenkins_status=$(ps -ef | grep jenkins | grep -v grep | grep -v jenkins_watchdog)
     if [ ! "$jenkins_status" ]; then
     echo $(date +%Y%m%d%H%M%S)  >> /root/logs/jenkins.log
     systemctl restart jenkins
     echo  $(date +%Y-%m-%-d-%H:%M:%S)  | mailx -s "jenkins 异常" he@jayce.icu 2>> /dev/null
-    sleep 10s
-  fi
-done
+fi
+# done
