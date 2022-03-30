40534037957625a7036a9a902259555317657d23
Jayce he <hejayce@gmail.com>
2022-03-30 10:39:05 +0800

    Update jenkins_watchdog.sh

--- jenkins_watchdog.sh
+++ jenkins_watchdog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # while true; do
-    jenkins_status=$(ps -ef | grep jenkins | grep -v grep | grep -v jenkins_watchdog)
-    if [ ! "$jenkins_status" ]; then
+echo $(date +%Y%m%d)  >> /root/logs/jenkins.log
+jenkins_status=$(ps -ef | grep jenkins | grep -v grep | grep -v jenkins_watchdog)
+if [ ! "$jenkins_status" ]; then
     echo $(date +%Y%m%d%H%M%S)  >> /root/logs/jenkins.log
     systemctl restart jenkins
     echo  $(date +%Y-%m-%-d-%H:%M:%S)  | mailx -s "jenkins 异常" he@jayce.icu 2>> /dev/null
